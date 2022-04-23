a62b01b62df99f6b7544f0ea848a6b975defc6d4
sabinaTT <tairsbn@gmail.com>
2022-04-23 04:12:17 -0700

    create Post Model and migrate

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    post_title = models.CharField(max_length=70)
+    post = models.TextField(max_length=6000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    img = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.post_title
+    
+    class Meta:
+        ordering = ['post_title']
+
